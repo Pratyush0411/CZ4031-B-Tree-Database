6455f728e7dd9522dd72aa765feb2509ec9cb309
CTAN210 <CTAN210@e.ntu.edu.sg>
2022-10-01 11:52:48 +0800

    Revert "trying exp 1"
    This reverts commit c53bde4a7136ea554d5d729f4f15ff7fb920e134.

--- entities/database.cpp
+++ entities/database.cpp
@@ -6,42 +6,33 @@
 #include "datablock.h"
 #include "iostream"
 
-Database::Database(int capacity, int blockSize)
-{
+Database::Database(int capacity, int blockSize) {
     this->MAXSIZE = capacity;
     this->blockSize = blockSize;
 }
 
-pair<DataBlock *, int> Database::addRecord(Record rec)
-{
-    cout << "size of record is: " << sizeof(rec) << endl;
-    if (sizeof(rec) > this->blockSize)
-    {
-        cout << "This record's size is larger than the block size" << endl;
-        return {NULL, -1};
+pair<DataBlock*, int> Database::addRecord(Record rec) {
+    if (sizeof(rec) > this->blockSize) {
+        cout << "This record's size is larger than the block size"<<endl;
+        return{NULL,-1};
     }
 
     // insert Record into a block that has unused space (for a deleted record);
-    if (freeBlocks.size())
-    {
-        DataBlock *blk = *freeBlocks.begin();
+    if (freeBlocks.size()) {
+        DataBlock* blk = *freeBlocks.begin();
         int recIndex = blk->insertRecord(rec);
-        if (blk->getNumFreeRecords() == 0)
-        {
+        if (blk->getNumFreeRecords() == 0) {
             freeBlocks.erase(freeBlocks.begin());
         }
-        // cout << "added to " << id << " block"<<endl;
-        return {blk, recIndex};
+        //cout << "added to " << id << " block"<<endl;
+        return { blk,recIndex };
     }
 
     // add new block
-    if (this->blocksList.size() == 0 || blocksList.back().hasCapacity(rec) == false)
-    {
-        cout << "Creating new block" << endl;
-        if (this->blockSize * (blocksList.size() + 1) > this->MAXSIZE)
-        {
+    if (this->blocksList.size() == 0 || blocksList.back().hasCapacity(rec)==false) {
+        if (this->blockSize *(blocksList.size()+1) > this->MAXSIZE) {
             cout << "Disk storage is full, cannot allocate another block" << endl;
-            return {NULL, -1};
+            return{ NULL,-1 };
         }
         DataBlock blk(this->blockSize);
         blocksList.push_back(blk);
@@ -49,37 +40,31 @@ pair<DataBlock *, int> Database::addRecord(Record rec)
 
     // insert Record into the last block;
     int recIndex = blocksList.back().insertRecord(rec);
-    // cout << "added to the last block"<<endl;
-    return {&blocksList.back(), recIndex};
+    //cout << "added to the last block"<<endl;
+    return{ &blocksList.back(),recIndex };
 }
 
-void Database::deleteRecordByNumvotes(int numVotes)
-{
-    for (int i = 0; i < blocksList.size(); i++)
-    {
-        if (blocksList[i].deleteRecordByNumvotes(numVotes))
-        {
+void Database::deleteRecordByNumvotes(int numVotes){
+    for (int i = 0; i < blocksList.size(); i++) {
+        if (blocksList[i].deleteRecordByNumvotes(numVotes)) {
             freeBlocks.insert(&blocksList[i]);
         }
     }
 }
 
-bool Database::deleteRecordfromBtree(DataBlock *blkPointer, int recordIndex)
-{
-    if (blkPointer->deleteRecordByIndex(recordIndex))
-    {
+bool Database::deleteRecordfromBtree(DataBlock* blkPointer, int recordIndex){
+    if (blkPointer->deleteRecordByIndex(recordIndex)) {
         freeBlocks.insert(blkPointer);
         return true;
     }
     return false;
 }
 
-int Database::getNumBlocks()
-{
+int Database::getNumBlocks() {
     return this->blocksList.size();
 }
 
-unsigned int Database::getSize()
-{
-    return this->blockSize * blocksList.size();
+unsigned int Database::getSize() {
+    return this->blockSize *blocksList.size();
 }
+
--- test/testBlock.cpp
+++ test/testBlock.cpp
@@ -7,29 +7,26 @@
 #include "../entities/database.h"
 #include "../entities/database.cpp"
 #include <cstring>
-#include <vector>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <iterator>
 
-template <typename Out>
-void split(const std::string &s, char delim, Out result)
-{
-    std::istringstream iss(s);
-    std::string item;
-    while (std::getline(iss, item, delim))
-    {
-        *result++ = item;
+int main(){
+
+    //DataBlock* blk1 = new DataBlock(200);
+    //blk1->insertRecord(*rec1);
+    //blk1->insertRecord(*rec2);
+    //blk1->printBlock();
+    ////blk1->deleteRecord(*rec1);
+    //blk1->printBlock();
+    Record* rec1 = new Record("t00000000",9.5,23);
+    //Record* rec2 = new Record("t00000001",9.0,20);
+    int DbSize = 1000;
+    int blockSize = 100;
+    Database* db = new Database(DbSize, blockSize);
+    pair<DataBlock*, int> p1 = db->addRecord(*rec1);
+    if (p1.first != NULL) {
+        cout << p1.first->recordList[p1.second].tconst << endl;
+        cout << p1.first->recordList[p1.second].avgRating << endl;
+        cout << p1.first->recordList[p1.second].numVotes << endl;
     }
-}
-
-std::vector<std::string> split(const std::string &s, char delim)
-{
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
 
 int main()
 {
